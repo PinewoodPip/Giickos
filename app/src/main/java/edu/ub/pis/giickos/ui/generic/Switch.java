960cf3ab554f70622c4e858966cd7edba4accc3a
Kamil <lekamilthemage@gmail.com>
2023-03-25 14:18:45 +0100

    Add Switch fragment

---
+++ app/src/main/java/edu/ub/pis/giickos/ui/generic/Switch.java
@@ -0,0 +1,70 @@
+package edu.ub.pis.giickos.ui.generic;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+
+import edu.ub.pis.giickos.R;
+
+// Wrapper fragment for a Switch view.
+public class Switch extends Fragment {
+    public static final String ARG_STATE = "State";
+
+    private CompoundButton.OnCheckedChangeListener listener = null;
+
+    public Switch() {
+        // Required empty public constructor
+    }
+
+    public static Switch newInstance(boolean state) {
+        Switch fragment = new Switch();
+        Bundle args = new Bundle();
+
+        args.putBoolean(ARG_STATE, state);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    public void setListener(CompoundButton.OnCheckedChangeListener listener) {
+        View view = getView();
+
+        this.listener = listener;
+
+        if (view != null) {
+            android.widget.Switch switchView = view.findViewById(R.id.view_switch);
+
+            switchView.setOnCheckedChangeListener(listener);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_generic_switch, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedBundle) {
+        android.widget.Switch switchView = view.findViewById(R.id.view_switch);
+
+        // Set state
+        switchView.setChecked(getArguments().getBoolean(ARG_STATE));
+
+        // Set checked listener, if one was provided
+        setListener(listener);
+    }
+}
