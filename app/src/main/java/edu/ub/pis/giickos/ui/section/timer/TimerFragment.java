c287fc7a5dcf92030d6c7372c2a66dad5dbc2459
ZhehanX <zhehanxiang2002@gmail.com>
2023-04-28 19:35:04 +0200

    Timer: basic function of timer (set timer, start timer, stop timer and reset timer)

--- app/src/main/java/edu/ub/pis/giickos/ui/section/timer/TimerFragment.java
+++ app/src/main/java/edu/ub/pis/giickos/ui/section/timer/TimerFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 import edu.ub.pis.giickos.R;
 
@@ -19,6 +24,22 @@ import edu.ub.pis.giickos.R;
  */
 public class TimerFragment extends Fragment {
 
+    private Button startPauseButton;
+    private Button selectTaskButton;
+
+    private Button resetButton;
+    private Button setMinuteButton;
+    private CheckBox detoxCheckbox;
+    private EditText editMinuteEdittext;
+    private TextView timerView;
+
+    private CountDownTimer countDownTimer;
+
+    private boolean istimerRunning;
+
+    private long startTimeInMillis;
+    private long timeLeftInMillis;
+    private long endTime;
 
     public TimerFragment() {
         // Required empty public constructor
@@ -40,15 +61,54 @@ public class TimerFragment extends Fragment {
     }
     public void onViewCreated(View view, Bundle bundle){
 
-        Button stopButton = view.findViewById(R.id.button_stop_timer);
-        Button selectTaskButton = view.findViewById(R.id.button_select_task);
-        CheckBox detoxCheckbox = view.findViewById(R.id.checkBox_detox);
+        startPauseButton = view.findViewById(R.id.button_start_timer);
+        selectTaskButton = view.findViewById(R.id.button_select_task);
+        setMinuteButton = view.findViewById(R.id.button_set_minutes);
+        resetButton = view.findViewById(R.id.button_reset_timer);
 
-        stopButton.setOnClickListener(new View.OnClickListener() {
+        detoxCheckbox = view.findViewById(R.id.checkBox_detox);
+        editMinuteEdittext = view.findViewById(R.id.edit_text_minutes);
+        timerView = view.findViewById(R.id.textView_timer);
+
+
+
+        setMinuteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                System.out.println("set button clickat");
+                String input = editMinuteEdittext.getText().toString();
+
+                long millisInput = Long.parseLong(input) * 60000;
+
+                if (input.length() >= 0 && millisInput >= 0){
+                    System.out.println("set metode correcte");
+                    setTime(millisInput);
+
+                    editMinuteEdittext.setText("");
+                }
+
+            }
+        });
+
+        startPauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO;
-                System.out.println("stopButton clicked");
+
+                System.out.println("startButton clicked");
+
+                if (istimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
             }
         });
 
@@ -69,6 +129,89 @@ public class TimerFragment extends Fragment {
         });
 
     }
+    private void setTime(long milliseconds) {
+        startTimeInMillis = milliseconds;
+        resetTimer();
+    }
+
+    private void startTimer() {
+        endTime = System.currentTimeMillis() + timeLeftInMillis;
+
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                istimerRunning = false;
+                updateWatchInterface();
+                //TODO: notification
+            }
+        }.start();
+
+        istimerRunning = true;
+        updateWatchInterface();
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        istimerRunning = false;
+        updateWatchInterface();
+    }
+
+    private void resetTimer() {
+        timeLeftInMillis = startTimeInMillis;
+        updateCountDownText();
+        updateWatchInterface();
+    }
+
+    private void updateCountDownText() {
+        int hours = (int) (timeLeftInMillis / 1000) / 3600;
+        int minutes = (int) ((timeLeftInMillis / 1000) % 3600) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted;
+        if (hours > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%d:%02d:%02d", hours, minutes, seconds);
+        } else {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%02d:%02d", minutes, seconds);
+        }
+
+        timerView.setText(timeLeftFormatted);
+
+    }
+
+    private void updateWatchInterface() {
+        if (istimerRunning) {
+            editMinuteEdittext.setVisibility(View.INVISIBLE);
+            setMinuteButton.setVisibility(View.INVISIBLE);
+            resetButton.setVisibility(View.INVISIBLE);
+            startPauseButton.setText("Pause");
+        } else {
+            editMinuteEdittext.setVisibility(View.VISIBLE);
+            setMinuteButton.setVisibility(View.VISIBLE);
+            startPauseButton.setText("Start");
+
+            if (timeLeftInMillis < 1000) {
+                startPauseButton.setVisibility(View.INVISIBLE);
+            } else {
+                startPauseButton.setVisibility(View.VISIBLE);
+            }
+
+            if (timeLeftInMillis < startTimeInMillis) {
+                resetButton.setVisibility(View.VISIBLE);
+            } else {
+                resetButton.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+
 
 
     @Override
--- app/src/main/java/edu/ub/pis/giickos/ui/section/timer/detox/DetoxFragment.java
+++ app/src/main/java/edu/ub/pis/giickos/ui/section/timer/detox/DetoxFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import edu.ub.pis.giickos.GiickosFragment;
 import edu.ub.pis.giickos.R;
@@ -34,6 +36,26 @@ public class DetoxFragment extends GiickosFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    public void onViewCreated(View view, Bundle bundle){
+
+        Button detoxStartButton = view.findViewById(R.id.button_detox_start);
+
+        detoxStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO;
+                System.out.println("detoxStartButton clicked");
+                if (detoxStartButton.getText().equals("STOP")){
+                    detoxStartButton.setText("START");
+                }else{
+                    detoxStartButton.setText("STOP");
+                }
+            }
+        });
+
 
     }
 
